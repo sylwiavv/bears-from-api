028cb013a229420ded5e7cc7b8bf8feda759ac03
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2021-12-21 23:51:58 +0100

    Added clear html before render beers

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const urlBase = "https://api.punkapi.com/v2/beers?page=";
-
 const container = document.querySelector('.container');
 const paginationPrev = document.querySelector('.prev');
 const paginationNext = document.querySelector('.next');
-const test = document.querySelector('.test');
 const page = document.querySelector('.page');
 const filterABV = document.getElementById("filterABV");
-let optionsABV = "";
-let perPage = '&per_page=5';
 
-// const API_URL_PAGINATION = 'https://api.punkapi.com/v2/beers?page=1&per_page=10';
+let optionsABV = "";
+let perPage = '&per_page=10';
 let pageNumber = 1;
 
 const nextPage = () => {
@@ -29,7 +26,6 @@ paginationPrev.addEventListener('click', prevPage);
 const error = (err) => console.log(err);
 
 const render = (beers) => {
-    let beerHtml = "";
     console.log(beers);
     if (!beers.length) return;
     const fragment = document.createDocumentFragment();
@@ -37,20 +33,20 @@ const render = (beers) => {
         const div = document.createElement('div');
         div.classList.add('beer');
         div.innerHTML = `
-        <h1>${abv}</h1>
+        <img class="beer--image" src="${imageURL}">
         <div class="beer--content">
             <h1 class="beer--title">${name}</h1>
             <p class="beer--tagline">${tagline}</p>
+            <p>ABV: <strong>${abv}</strong></p>
             <p class="beer--description">${description}</p>
         </div>
-        <img class="beer--image" src="${imageURL}">
         `;
         fragment.appendChild(div);
     });
-
-    page.innerHTML = pageNumber;
+    container.textContent = '';
     container.appendChild(fragment);
 
+    page.innerHTML = pageNumber;
     if (pageNumber === 1) {
         paginationPrev.setAttribute("disabled", "");
     } else if (pageNumber === 6) {
