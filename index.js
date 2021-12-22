3089637ff5074d8c6951e431c85f654a29a21ca5
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2021-12-22 17:07:25 +0100

    Removed unnecessary code

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const urlBase = new URL("https://api.punkapi.com/v2/beers?page=1");
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
+
 const container = document.querySelector('.container');
 const paginationPrev = document.querySelector('.prev');
 const paginationNext = document.querySelector('.next');
+let tekst = document.querySelector('.test');
 const page = document.querySelector('.page');
 const filterABV = document.getElementById("filterABV");
-
 let optionsABV = "";
-let perPage = '&per_page=10';
+let perPage = '&per_page=5';
 let pageNumber = 1;
 
 const nextPage = () => {
@@ -30,15 +31,22 @@ const render = (beers) => {
     if (!beers.length) return;
     const fragment = document.createDocumentFragment();
     beers.forEach(({ name, tagline, description, abv, image_url: imageURL }) => {
+        // const h1 = document.createElement('h1');
+        // test.append(h1);
+        // h1.innerHTML = `${name}`;
+        // console.log((h1));
+        // test.replaceChild(h1);
         const div = document.createElement('div');
         div.classList.add('beer');
         div.innerHTML = `
-        <img class="beer--image" src="${imageURL}">
+        <h1>${abv}</h1>
+        <div class="bear">
         <div class="beer--content">
             <h1 class="beer--title">${name}</h1>
             <p class="beer--tagline">${tagline}</p>
-            <p>ABV: <strong>${abv}</strong></p>
             <p class="beer--description">${description}</p>
+        </div>
+        <img class="beer--image" src="${imageURL}">
         </div>
         `;
         fragment.appendChild(div);
@@ -47,20 +55,14 @@ const render = (beers) => {
     container.textContent = '';
     container.appendChild(fragment);
 
-    console.log(pageNumber);
-
     page.innerHTML = pageNumber;
     if (pageNumber === 1) {
         paginationPrev.setAttribute("disabled", "");
-        paginationPrev.classList.add('hidden');
     } else if (pageNumber === 6) {
         paginationNext.setAttribute("disabled", "");
-        paginationNext.classList.add('hidden');
     } else {
         paginationPrev.removeAttribute("disabled");
         paginationNext.removeAttribute("disabled");
-        paginationNext.classList.remove('hidden');
-        paginationPrev.classList.remove('hidden');
     }
 }
 
@@ -73,16 +75,13 @@ filterABV.addEventListener("change", e => {
             optionsABV = "";
             break
         case "weak":
-            // optionsABV = "&abv_lt=4.6";
-            urlBase.searchParams.append("&abv_lt", "4.6");
+            optionsABV = "&abv_lt=4.6";
             break
-        // case "medium":
-        //     optionsABV = "&abv_gt=4.5&abv_lt=7.6";
-        //     urlBase.searchParams.append("abv_lt", "4.6");
-        //     break
+        case "medium":
+            optionsABV = "&abv_gt=4.5&abv_lt=7.6";
+            break
         case "strong":
-            // optionsABV = "&abv_gt=7.5";
-            urlBase.searchParams.append("&abv_gt", "7.5");
+            optionsABV = "&abv_gt=7.5";
             break
     }
     pageNumber = 1;
@@ -90,11 +89,10 @@ filterABV.addEventListener("change", e => {
 })
 
 async function getBeers() {
-    // const url = urlBase + pageNumber + optionsABV + perPage;
-    const beerPromise = await fetch(urlBase);
+    const url = urlBase + pageNumber + optionsABV + perPage;
+    const beerPromise = await fetch(url);
     const beers = await beerPromise.json();
-    // console.log(url);
-    console.log(urlBase.href);
+    console.log(url);
     render(beers);
 }
 
