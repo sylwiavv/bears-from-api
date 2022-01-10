8d066e83a404529ab7c065ff2885ebe317a6e987
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-01-10 15:55:28 +0100

    Added title adn styles for filters, chqnged pagination appearance, added media queries

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-let url = "https://api.punkapi.com/v2/beers?per_page=10";
+let url = "https://api.punkapi.com/v2/beers?per_page=9";
 let currentUrl;
 const container = document.querySelector('.container');
 const paginationPrev = document.querySelector('.prev');
 const paginationNext = document.querySelector('.next');
-const page = document.querySelector('.page');
+const page = document.querySelector('.page--number');
 const filterABV = document.getElementById("filterABV");
 // let perPage = '&per_page=5';
 let pageNumber = 1;
+const beerDescription = document.querySelectorAll('beer');
 
-const error = (err) => console.log(err);
+// const error = (err) => console.log(err);
 
 const render = (beers) => {
-    console.log(beers);
     if (!beers.length) return;
     const fragment = document.createDocumentFragment();
     beers.forEach(({name, tagline, description, abv, image_url: imageURL}) => {
         const div = document.createElement('div');
         div.classList.add('beer');
         div.innerHTML = `
-        <h1>${abv}</h1>
-        <div class="bear">
+        <img class="beer--image animate-pop" src="${imageURL}">
         <div class="beer--content">
-            <h1 class="beer--title">${name}</h1>
+            <h3 class="beer--title">${name}</h3>
+            <p class="beer--abv"><span>Abv:</span> ${abv}%</p>
             <p class="beer--tagline">${tagline}</p>
-            <p class="beer--description">${description}</p>
-        </div>
-        <img class="beer--image" src="${imageURL}">
         </div>
         `;
         fragment.appendChild(div);
@@ -33,70 +30,73 @@ const render = (beers) => {
 
     container.textContent = '';
     container.appendChild(fragment);
-
     page.innerHTML = pageNumber;
     if (pageNumber === 1) {
         paginationPrev.setAttribute("disabled", "");
-    } else if (pageNumber === 6) {
+        paginationPrev.classList.add('disabled');
+        paginationNext.classList.remove('disabled');
+        paginationNext.removeAttribute("disabled");
+    } else if (pageNumber === 5) {
         paginationNext.setAttribute("disabled", "");
+        paginationNext.classList.add('disabled');
     } else {
         paginationPrev.removeAttribute("disabled");
-        paginationNext.removeAttribute("disabled");
+        paginationPrev.classList.remove('disabled');
+        paginationNext.classList.remove('disabled');
     }
+    console.log(pageNumber);
 }
 
 filterABV.addEventListener("change", e => {
-    const input = document.querySelector('.filter');
-    console.log(input.value);
     const value = e.target.value;
     switch (value) {
         case "all":
-            getBeers(urlOnLoad);
+            currentUrl = new URL(url);
             break
         case "weak":
             currentUrl = new URL(url);
             currentUrl.searchParams.append('abv_lt', '4.6');
-            // console.log(currentUrl.href + ' weak');
             break
         case "medium":
             currentUrl = new URL(url);
             currentUrl.searchParams.append('abv_gt', '4.5');
             currentUrl.searchParams.append('abv_lt', '7.6');
-            nextPage();
-            // console.log(currentUrl.href + ' medium');
             break
         case "strong":
             currentUrl = new URL(url);
             currentUrl.searchParams.append('abv_gt', '7.5');
-            // console.log(currentUrl.href + ' strong');
             break
     }
-    console.log(currentUrl);
     pageNumber = 1;
     getBeers(currentUrl);
 });
 
 const nextPage = () => {
     pageNumber++;
-    urlOnLoad.searchParams.set('page', pageNumber);
+    currentUrl.searchParams.set('page', pageNumber);
     getBeers(currentUrl);
-    console.log(currentUrl);
 }
 const prevPage = () => {
     pageNumber--;
-    urlOnLoad.searchParams.set('page', pageNumber);
-    getBeers(urlOnLoad);
+    currentUrl.searchParams.set('page', pageNumber);
+    getBeers(currentUrl);
+}
+
+container.classList.add('on-load');
+
+setTimeout(function(){
+    container.classList.remove('on-load');
+    currentUrl = new URL(url);
+    getBeers(currentUrl);
+}, 1500);
+
+async function getBeers(currentUrl) {
+    const beerPromise = await fetch(currentUrl);
+    const beers = await beerPromise.json();
+    render(beers);
+    container.classList.add('animate-pop');
 }
 
 paginationNext.addEventListener('click', nextPage);
 paginationPrev.addEventListener('click', prevPage);
 
-async function getBeers(currentUrl) {
-    console.log(currentUrl);
-    const beerPromise = await fetch(currentUrl);
-    const beers = await beerPromise.json();
-    render(beers);
-}
-
-let urlOnLoad = new URL(url);
-getBeers(urlOnLoad);
